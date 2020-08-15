e79304e64041554c70c6982e7ef28cf8ddd63522
Joe van Bohemen <joseph.vanbohemen@milkbooks.com>
2020-08-16 10:14:07 +1200

    e

--- test/discord.test.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-require('dotenv').config({
-  path: path.join(__dirname, '../.env')
-})
-const D = require('../dist/clients/discord').default
-const d = new D()
-d.getUser('191392978588139530')
-.then(console.log)
-.catch(console.log)
